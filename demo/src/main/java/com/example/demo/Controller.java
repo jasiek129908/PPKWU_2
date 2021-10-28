e739437a8c9bdb20aa713bb291123a740f74dd2f
Jan Oleksik <222529@edu.p.lodz.pl>
2021-10-28 12:25:44 +0200

    added dummy implementation

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    
+
+    @GetMapping("/string")
+    public String getInformationAboutString(@RequestParam String text){
+        return text;
+    }
 }
