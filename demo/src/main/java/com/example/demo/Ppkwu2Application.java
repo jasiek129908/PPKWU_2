7fe2425fa65999e0f51e6019963d683f82f18d3c
Jan Oleksik <222529@edu.p.lodz.pl>
2021-10-28 12:19:56 +0200

    project init

---
+++ demo/src/main/java/com/example/demo/Ppkwu2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppkwu2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ppkwu2Application.class, args);
+	}
+
+}
